3d734d6ac4477b77e5ca70e3ec4d1e0f7d6ef757
Maksim Egorov <m.egorov193@gmail.com>
2023-11-01 10:21:49 +0400

    fix

--- src/main/java/guru/qa/git/Main.java
+++ src/main/java/guru/qa/git/Main.java
@@ -3,5 +3,7 @@ package guru.qa.git;
 public class Main {
     public static void main (String[] args) {
         System.out.print("Hello qa.guru");
+        System.out.print("Hello qa.guru");
+        System.out.print("Hello qa.guru");
     }
 }
