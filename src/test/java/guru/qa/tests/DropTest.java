e6e63c02ca41fa6ddcc8efaeb2cf8f0aee53618a
Maksim Egorov <m.egorov193@gmail.com>
2023-11-07 20:18:44 +0400

    hw5.hover and drop tests

---
+++ src/test/java/guru/qa/tests/DropTest.java
@@ -0,0 +1,37 @@
+package guru.qa.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Point;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DropTest {
+
+    @BeforeEach
+    void setUp() {
+        Configuration.holdBrowserOpen = true;
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+    }
+
+    @Test
+    void dropTest() {
+
+        //2. (опциональное) Запрограммируйте Drag&Drop с помощью Selenide.actions()
+        // - Откройте https://the-internet.herokuapp.com/drag_and_drop
+        // - Перенесите прямоугольник А на место В
+        // - Проверьте, что прямоугольники действительно поменялись
+        // - В Selenide есть команда $(element).dragAndDrop($(to-element)),
+        // проверьте работает ли тест, если использовать её вместо actions()
+
+        Point pointA = $("#column-a").getLocation();
+        Point pointB = $("#column-b").getLocation();
+        actions().dragAndDropBy($("#column-a"),
+                pointB.getX() - pointA.getX(),
+                pointB.getY() - pointA.getY()).perform();
+        $("#column-a").shouldHave(text("B"));
+
+    }
+}
---
+++ src/test/java/guru/qa/tests/GitHoverTest.java
@@ -0,0 +1,21 @@
+package guru.qa.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GitHoverTest {
+    @Test
+    void hoverTest() {
+        //1. На главной странице GitHub выберите меню Solutions -> Enterprize с помощью команды hover для Solutions.
+        // Убедитесь что загрузилась нужная страница (например что заголовок - "Build like the best."
+        open("https://github.com");
+        $("[aria-label=Global]").$("ul").$$("li")
+                .findBy(text("Solutions")).hover();
+        $("ul").$("[aria-labelledby=solutions-for-heading]").$(withText("Enterprise")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform."));
+    }
+}
