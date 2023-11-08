f0673861229e471ece521adf5989db31064bd822
Maksim Egorov <m.egorov193@gmail.com>
2023-11-08 13:26:40 +0400

    hw5.upd

--- src/test/java/guru/qa/tests/DropTest.java
+++ src/test/java/guru/qa/tests/DropTest.java
@@ -1,11 +1,12 @@
 package guru.qa.tests;
 
+import com.codeborne.selenide.DragAndDropOptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Point;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class DropTest {
 
@@ -17,19 +18,20 @@ public class DropTest {
     @Test
     void dropTest() {
 
-        //2. (опциональное) Запрограммируйте Drag&Drop с помощью Selenide.actions()
-        // - Откройте https://the-internet.herokuapp.com/drag_and_drop
-        // - Перенесите прямоугольник А на место В
-        // - Проверьте, что прямоугольники действительно поменялись
-        // - В Selenide есть команда $(element).dragAndDrop($(to-element)),
-        // проверьте работает ли тест, если использовать её вместо actions()
+//        2. (опциональное) Запрограммируйте Drag&Drop с помощью Selenide.actions()
+//         - Откройте https://the-internet.herokuapp.com/drag_and_drop
+//         - Перенесите прямоугольник А на место В
+//         - Проверьте, что прямоугольники действительно поменялись
+//         - В Selenide есть команда $(element).dragAndDrop($(to-element)),
+//         проверьте работает ли тест, если использовать её вместо actions()
+//        Point pointA = $("#column-a").getLocation();
+//        Point pointB = $("#column-b").getLocation();
+//        actions().dragAndDropBy($("#column-a"),
+//                pointB.getX() - pointA.getX(),
+//                pointB.getY() - pointA.getY()).perform();
+//        $("#column-a").shouldHave(text("B"));
 
-        Point pointA = $("#column-a").getLocation();
-        Point pointB = $("#column-b").getLocation();
-        actions().dragAndDropBy($("#column-a"),
-                pointB.getX() - pointA.getX(),
-                pointB.getY() - pointA.getY()).perform();
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
         $("#column-a").shouldHave(text("B"));
-
     }
 }
