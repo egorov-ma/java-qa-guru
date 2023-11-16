efa8c8305161b82b798d3725338837439c325980
Maksim Egorov <m.egorov193@gmail.com>
2023-11-16 13:17:31 +0400

    fix dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,12 +19,9 @@ tasks {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("com.codeborne:selenide:6.19.1")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testImplementation("org.slf4j:slf4j-simple:2.0.9")
-    implementation("org.slf4j:slf4j-api:2.0.9")
 }
 
 tasks.test {
